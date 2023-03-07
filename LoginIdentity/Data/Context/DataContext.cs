69c3f28bc059cd8cd746114e00a9efd8919ef109
Leonardo Coutinho <leoctc@hotmail.com>
2023-03-07 19:32:10 -0300

    removido

---
+++ LoginIdentity/Data/Context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginIdentity.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginIdentity.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("DataSource=login.db;Cache=Shared");
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ LoginIdentity/Data/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginIdentity.Data.Entities
+{
+    public class Usuario : IdentityUser
+    {
+        private string Login { get; set; }
+
+        private string Senha { get; set; }
+    }
+}
